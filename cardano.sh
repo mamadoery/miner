6486ac03b10910533396a84644074c1d6effe660
mamadoery <mamadoery@gmail.com>
2021-06-16 07:25:16 -0300

    Create cardano.sh

---
+++ cardano.sh
@@ -0,0 +1,6 @@
+# cardano
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz
+tar xf xmrig-6.12.2-linux-x64.tar.gz
+cd xmrig-6.12.2
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u ADA:DdzFFzCqrht5B9s56JVMC6Ps1HRSNMU1zqBySDf8sh6rTpxtTV7nNeiD9dcHsSPZ3zcf3YwsJhty7hvBFfJxFymMHbNFUdSqXFzZwVNC.BITBUCKET -p x --cpu 4
